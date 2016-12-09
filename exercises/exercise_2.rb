dabe07f784081c6704d1505021664fb36cdc8d76
DeeKeilholz <deekeilholz@gmail.com>
2016-12-09 20:59:17 +0000

    adds first three exercises

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,15 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
+
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.update(name: 'Kelowna')
+
+puts @store1.name
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,13 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
+### Exercise 3: Delete the third store
+
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.find(3)
+
+@store3.destroy
+
+puts Store.count
